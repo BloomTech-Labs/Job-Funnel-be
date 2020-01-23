866a9813a08419df0298a1037811aff211954bde
Anthony Hill <hillan1152@gmail.com>
2020-01-22 19:26:16 -0500

    Test Co-authored-by: mauricio degregori2@gmail.com Co-authored-by: jordan jordan.j.miller@live.com

--- knexfile.js
+++ knexfile.js
@@ -6,40 +6,48 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: './data/jobs.db3'
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory:'./data/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+				// runs after a connection is made to the sqlite engine
+				conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+			}
     }
   },
 
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3',
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory:'./data/seeds'
+    },
+  },
 };
---
+++ users/users-helpers.js
@@ -0,0 +1,19 @@
+// module.exports = user => {
+//     let errors = [];
+    
+
+// 	if (!user.username || user.username.length < 4) {
+// 		errors.push("Please provide a password with at least 2 characters");
+// 	}
+
+// 	if (!user.password || user.password.length < 4) {
+// 		errors.push("Please provide a password with at least 4 characters");
+// 	}
+
+// 	return {
+// 		isSuccessful: errors.length > 0 ? false : true,
+// 		errors
+// 	}
+// };
+
+
