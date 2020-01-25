a7049932128a7e4d299cd023aaacf797f2140d2c
Jordan Miller <jordan.j.miller@live.com>
2020-01-25 13:03:20 -0800

    enabled user router, added debug router

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const helmet = require('helmet');
 
 const authenticate = require('./auth/authenticate-middleware.js');
 const authRouter = require('./auth/auth-router.js');
-// const userRouter = require('./routers/users-router.js');
+const userRouter = require('./routers/users-router.js');
+const debugRouter = require('./routers/debug-router.js');
 
 const server = express();
 
@@ -17,7 +18,8 @@ server.use(cors());
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
-// server.use('/api/users', authenticate, userRouter);
+server.use('/api/users', authenticate, userRouter);
+server.use('/api/debug', authenticate, debugRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json({message: 'hi'});
