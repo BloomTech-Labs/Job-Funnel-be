339f168e6afdcf27719c78fd9f223781781298f9
Jordan Miller <jordan.j.miller@live.com>
2020-01-25 11:22:08 -0800

    revenue column is an integer, fixing

--- seeds/01_companies.js
+++ seeds/01_companies.js
@@ -5,19 +5,19 @@ exports.seed = function(knex) {
         name: "IBM",
         description: "a big tech company",
         size: "large",
-        revenue: "79 billion"
+        revenue: 79000000000
       },
       {
         name: "apple",
         description: "maker of expensive ",
         size: "extra large",
-        revenue: "258 billion"
+        revenue: 258000000000
       },
       {
         name: "amazon",
         description: "seller of stuff",
         size: "MEGA",
-        revenue: "232 billion"
+        revenue: 232000000000
       }
     ],
     "id"
