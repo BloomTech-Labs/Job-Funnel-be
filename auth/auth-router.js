878d86a668aa9ba08eb246e050161647a7421fd7
Jordan Miller <jordan.j.miller@live.com>
2020-01-25 11:14:50 -0800

    merge conflict snuck back in, fixed again

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,14 +5,9 @@ const userDb = require("../data/users-model.js");
 const generateToken = require("./token.js");
 
 router.post("/register", async (req, res) => {
-  const user = ({
-    first_name,
-    last_name,
-    email,
-    password,
-    user_type
-  } = req.body);
+  const user = ({ first_name, last_name, email, password, user_type } = req.body);
   console.log("registering ", first_name, last_name);
+
   for (let val in user) {
     if (typeof user[val] === "string") {
       user[val] = user[val].toLowerCase();
@@ -22,26 +17,19 @@ router.post("/register", async (req, res) => {
   try {
     if (!(email && password)) {
       throw 1;
+    }if (!first_name || !last_name) {
+      throw 2;
+    }if (!user_type) {
+      throw 3;
     }
-
     const foundEmail = await db("users")
       .where({ email: user.email })
       .first();
-
     if (foundEmail) {
-      throw 2;
-    }
-    if (!first_name || !last_name) {
-      throw 3;
-    }
-    if (!user_type) {
       throw 4;
     }
 
-    const [id] = await userDb.add({
-      ...user,
-      password: bcrypt.hashSync(password, 12)
-    });
+    const [id] = await userDb.add({...user, password: bcrypt.hashSync(password, 12)});
 
     const response = await db("users")
       .select("id", "email")
@@ -52,14 +40,12 @@ router.post("/register", async (req, res) => {
   } catch (err) {
     if (err === 1) {
       res.status(400).json({ message: `Email and password are required.` });
-    } else if (err === 2) {
-      res.status(409).json({
-        message: `There is already an account associated with that email`
-      });
-    } else if (err === 3) {
+    }  else if (err === 2) {
       res.status(400).json({ message: `First and last names are required` });
-    } else if (err === 4) {
+    } else if (err === 3) {
       res.status(400).json({ message: `User type is required` });
+    } else if (err === 4) {
+      res.status(409).json({ message: `There is already an account associated with that email` });
     } else {
       console.log(err);
       res
@@ -69,42 +55,20 @@ router.post("/register", async (req, res) => {
   }
 });
 
-<<<<<<< HEAD
 router.post('/login', async (req, res) => {
-    const {email, password} = req.body;
-    if(email && password){
-        const user = await db('users as u').where({'u.email': email.toLowerCase()})
-            .select('u.*')
-            .first();
-        if(user && bcrypt.compareSync(password, user.password)){
-            const token = await generateToken(user);
-            res.status(200).json({message: `Welcome ${user.first_name}`, token, user: {...user, password: undefined}});
-        }else{
-            res.status(403).json({message: 'Invalid email or password'});
-        }
-    }else{
-        res.status(400).json({message: 'Please provide an email and password'});
-=======
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  if (email && password) {
-    const user = await db("users as u")
-      .where({ "u.email": email.toLowerCase() })
-      .select("u.*")
-      .first();
-    if (user && bcrypt.compareSync(password, user.password)) {
-      const token = await generateToken(user);
-      res.status(200).json({
-        message: `Welcome ${user.name}`,
-        token,
-        user: { ...user, password: undefined }
-      });
-    } else {
-      res.status(403).json({ message: "Invalid email or password" });
->>>>>>> 78e47803812accdd4b8018c9fd1b0333aea2319a
-    }
-  } else {
-    res.status(400).json({ message: "Please provide an email and password" });
+  const {email, password} = req.body;
+  if(email && password){
+      const user = await db('users as u').where({'u.email': email.toLowerCase()})
+          .select('u.*')
+          .first();
+      if(user && bcrypt.compareSync(password, user.password)){
+          const token = await generateToken(user);
+          res.status(200).json({message: `Welcome ${user.first_name}`, token, user: {...user, password: undefined}});
+      }else{
+          res.status(403).json({message: 'Invalid email or password'});
+      }
+  }else{
+      res.status(400).json({message: 'Please provide an email and password'});
   }
 });
 
