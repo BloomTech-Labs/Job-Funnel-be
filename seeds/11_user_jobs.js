a0f9ecb648c620fbb14bcf2fd2761a9138a357d8
Jordan Miller <jordan.j.miller@live.com>
2020-01-25 10:36:10 -0800

    removing period used to get around bad unique constraint in table

--- seeds/11_user_jobs.js
+++ seeds/11_user_jobs.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex) {
   return knex("user_jobs").insert(
     [
-      { job_id: 1, user_id: 1, status: "saved." },
+      { job_id: 1, user_id: 1, status: "saved" },
       { job_id: 2, user_id: 2, status: "saved" },
       { job_id: 3, user_id: 3, status: "applied" }
     ],
